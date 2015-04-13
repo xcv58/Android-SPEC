e98ce2f7861fae3658d599c29438756afdeac69d
xcv58 <chenyihonglove@gmail.com>
2015-04-13 15:22:37 -0400

    prepare to implemnt control flow

--- app/src/main/java/com/xcv58/spec/android/services/SPECIntentService.java
+++ app/src/main/java/com/xcv58/spec/android/services/SPECIntentService.java
@@ -2,7 +2,6 @@ package com.xcv58.spec.android.services;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -16,45 +15,6 @@ import com.xcv58.spec.android.utils.Utils;
  * helper methods.
  */
 public class SPECIntentService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "com.xcv58.spec.android.services.action.FOO";
-    private static final String ACTION_BAZ = "com.xcv58.spec.android.services.action.BAZ";
-
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "com.xcv58.spec.android.services.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "com.xcv58.spec.android.services.extra.PARAM2";
-
-    /**
-     * Starts this service to perform action Foo with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, SPECIntentService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, SPECIntentService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
     public SPECIntentService() {
         super("SPECIntentService");
     }
@@ -70,44 +30,6 @@ public class SPECIntentService extends IntentService {
                     Log.d(Utils.TAG, key + ": " + bundle.getString(key));
                 }
             }
-            long startTime = System.currentTimeMillis();
-            while (true) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (System.currentTimeMillis() - startTime > 5000) {
-                    break;
-                }
-            }
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
-            }
         }
-    }
-
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
